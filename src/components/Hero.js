39ffe2fdf6d332f5821c90da5a7ada0ac04470c6
dominggo1999 <arnolddominggo19@gmail.com>
2021-04-28 22:46:01 +0700

    responsive main hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from './Button';
+import MainHero from './MainHero';
 
 const Hero = ({ type }) => {
   return (
@@ -8,27 +9,7 @@ const Hero = ({ type }) => {
       <div className="container">
         {
           type === 'main'
-            ? (
-              <>
-                <div className="left">
-                  <img
-                    src="https://res.cloudinary.com/dv8aesvfs/image/upload/v1619429006/hero-1_mullcc.png"
-                    alt="Hero"
-                    className="hero-image"
-                  />
-                </div>
-                <div className="right">
-                  <h3 className="main-sub-1">Welcome to our</h3>
-                  <h1 className="main-sub-2">Delicious</h1>
-                  <h1 className="main-sub-3">Food Collection</h1>
-                  <p className="main-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
-                  <div className="buttons">
-                    <Button>About Us</Button>
-                    <Button type="orange">Our Menu</Button>
-                  </div>
-                </div>
-              </>
-            )
+            ? <MainHero />
             : <p>Under construction</p>
         }
       </div>
