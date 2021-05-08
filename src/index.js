0ae96016f06d4a87e68f83d1f7aa4c741eed044c
dominggo1999 <arnolddominggo19@gmail.com>
2021-05-08 13:24:15 +0700

    scroll to top

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import { Provider } from 'react-redux';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
+import ScrollToTop from './components/ScrollToTop';
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </Provider>,
